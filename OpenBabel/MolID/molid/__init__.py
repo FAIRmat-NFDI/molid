e7801eff1f44d9373077eb0c09d96c272071c63a
tdenell <denell@physik.hu-berlin.de>
2025-02-05 16:25:12 +0100

    fixing init import for normalizer

--- OpenBabel/MolID/molid/__init__.py
+++ OpenBabel/MolID/molid/__init__.py
@@ -2,6 +2,6 @@
 from .db import database, search
 from .pubchemproc import pubchem, file_handler, query
 from .utils import disk_utils, ftp_utils
-from .query import query_pubchem_database
+from .pubchemproc.query import query_pubchem_database
 
 __all__ = ["db", "pubchemproc", "utils", "query_pubchem_database"]
