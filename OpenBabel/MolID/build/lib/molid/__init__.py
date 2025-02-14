0937803a1d6e9b308f351687ef5b80ede1cdd04c
tdenell <denell@physik.hu-berlin.de>
2025-02-14 11:38:51 +0100

    Refactored repository structure: moved MolID out of OpenBabel

--- OpenBabel/MolID/build/lib/molid/__init__.py
+++
@@ -1,7 +0,0 @@
-# Import submodules for easier access
-from .db import database, search
-from .pubchemproc import pubchem, file_handler, query
-from .utils import disk_utils, ftp_utils
-from .query import query_pubchem_database
-
-__all__ = ["db", "pubchemproc", "utils", "query_pubchem_database"]
