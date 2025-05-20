34accb93c842c2a93ec7815ce2be5a67c42bc7b6
tdenell <denell@physik.hu-berlin.de>
2025-05-20 14:11:42 +0200

    added missing __init__.py

---
+++ archive/molid/utils/__init__.py
@@ -0,0 +1,4 @@
+from .disk_utils import check_disk_space, is_disk_space_sufficient
+from .ftp_utils import get_total_files_from_ftp
+
+__all__ = ["check_disk_space", "is_disk_space_sufficient", "get_total_files_from_ftp"]
