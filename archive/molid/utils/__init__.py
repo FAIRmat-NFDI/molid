05e02fda69b0c2857ac427f3274cc373fa91981b
tdenell <denell@physik.hu-berlin.de>
2025-05-20 16:20:28 +0200

    removed archive folder

--- archive/molid/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .disk_utils import check_disk_space, is_disk_space_sufficient
-from .ftp_utils import get_total_files_from_ftp
-
-__all__ = ["check_disk_space", "is_disk_space_sufficient", "get_total_files_from_ftp"]
